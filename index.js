15e06d89271d66b2dbdb96d9c393bed37014da69
Luis Furtado <lucho_l.f@hotmail.com>
2023-11-14 21:27:56 -0300

    paginas base

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import router from './routes/index.js';
+
 
 const app = express();
 
@@ -6,11 +8,26 @@ const app = express();
 // Definir puerto
 const port = process.env.PORT = 4000;
 
-
 // Habilitar PUG
 app.set('view engine', 'pug');
 
 
+// Obtener el año actaul
+app.use( (req, res, next) => {
+    const year = new Date();
+
+    res.locals.actualYear = year.getFullYear();
+    res.locals.nombreSitio = 'Agencia de Viajes';
+    next();
+});
+
+
+// Definir la carpeta publica
+app.use(express.static('public'));
+
+// Agregar Router
+app.use('/', router);
+
 app.listen(port, () => {
     console.log(`El servidor esta funcionando en el puerto ${port}`);
-})
+});
