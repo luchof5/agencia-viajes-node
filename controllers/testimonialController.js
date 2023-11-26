6e178bbf498a6558ac94818e4d78fd478198a550
Luis Furtado <lucho_l.f@hotmail.com>
2023-11-25 21:37:24 -0300

    validacion formulario

--- controllers/testimonialController.js
+++ controllers/testimonialController.js
@@ -1,7 +1,38 @@
 
 const guardarTestimonial = (req, res) => {
 
-        console.log(req.body);
+    // Validar...
+
+    const { nombre, correo, mensaje } = req.body;
+
+    const errores = [];
+
+    if(nombre.trim() === '') {
+        errores.push({mensaje: 'El nombre esta vacio'});
+    }
+
+    if(correo.trim() === '') {
+        errores.push({mensaje: 'El correo esta vacio'});
+    }
+
+    if(mensaje.trim() === '') {
+        errores.push({mensaje: 'El mensaje esta vacio'});
+    }
+
+    if (errores.length > 0) {
+        
+        // Mostrar la vista con errores
+        res.render('testimoniales', {
+            pagina: 'Testimoniales',
+            errores,
+            nombre,
+            correo,
+            mensaje
+        })
+    } else {
+        // Almacenar en la base de datos
+
+    }
 
 }
 
